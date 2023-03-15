0c74e3a2b2465f0b0cd8cc56bff70f62204a80e7
Youngsu Kim <126743125+Createcoding@users.noreply.github.com>
2023-03-15 16:05:36 +0900

    for 반복문 정리

--- 1. 자바스크립트 기초/8. 반복문for.js
+++ 1. 자바스크립트 기초/8. 반복문for.js
@@ -1,9 +1,15 @@
 
 
-// 횟수 지정 반복문 5바퀴 루프
-var n = 1;
+var i = 1; // 1. 제어변수 선언
 
-while(n <= 5){
-    console.log(`hello`);
-    n++;
+while ( i <= 5 ) { // 2. 조건식
+    
+    console.log('hello'); // 3. 반복실행
+    i++; // 4. 증감식
+}
+
+
+// 횟수 지정 반복문 5바퀴 루프
+for (var i = 1; i <= 5; i++) {
+    console.log('hello');
 }
