5842dd2d31dc1a03c5d3ab749b9143db4b83f1cc
Youngsu Kim <126743125+Createcoding@users.noreply.github.com>
2023-03-20 10:45:41 +0900

    객체 기초 커밋

--- 3. 객체/1. 객체 기초.js
+++ 3. 객체/1. 객체 기초.js
@@ -4,72 +4,89 @@
 var dog = {
     // 프로퍼티 작성
     'name': '뽀삐',
-    //key는 변수처럼 문자열 사용가능
-    //value는 아무거나 다넣어도된다. 배열,함수,null 등등
-    playWithChild: function() {},
-    kind: '진돗개',
     age: 3,
-    for: 11,
-    // 'for waiting': 11,
-    //문자열로 처리하면 띄어쓰기도 가능.
+    //'for waiting': 11,
     injection: true,
-    favorite: ['산책','간식'],
-    
+    favorite: ['산책', '간식'],
+    hate: null,
+    kind: '진돗개',
 };
-console.log(typeof dog);
-//객체에 저장된 데이터 참조하는방법
+
+
+
+"hello".slice();
+
+var cat = {
+    'name': '콩순이',
+    age: 2,
+    injection: true,
+    favorite: ['낮잠자기', '방어지럽히기'],
+    kind: '블랙러시안',
+};
+
+console.log(typeof cat);
+
+// 객체에 저장된 데이터 참조
 console.log(dog.name);
-dog.favorite.push('강아지');
+console.log(cat.age);
+console.log(cat.favorite);
 
-dog.favorite.slice(1).splice(1,1);
+console.log(dog.favorite[0]);
 
-//프로퍼티 참조 2번째방법
-console.log('=======================');
+dog.favorite.push('꼬리 흔들기');
+console.log(dog);
+
+dog.favorite.slice(1).splice(1, 1);
+
+// 프로퍼티 참조 2
+console.log('========================');
+console.log(dog.injection);
 console.log(dog['injection']);
 
-//key 를 변수에 저장
+// key를 변수에 저장
 var fv = 'favorite';
-//변수에 담아서 참조하니까 '' 를빼준것
-console.log(dog[fv]);
+console.log(cat[fv]);
 
-//프로퍼티 값 수정
+
+// 프로퍼티 값 수정
 dog.age = 4;
-//console.log(dog.age);
-dog.favorite[1] = '실뭉치';
-//console.log(dog.favorite);
+cat.favorite[1] = '실뭉치';
 
 dog['injection'] = false;
 console.log(dog);
 
-//프로퍼티 동적 추가
-dog.owner = '김철수';
-console.log(dog);
 
-//프로퍼티 삭제
-delete dog.owner;
-console.log(dog);
+// 프로퍼티 동적 추가
+cat.owner = '김철수'; // 추가
+cat.owner = '박영희'; // 수정
+console.log(cat);
 
-//프로퍼티 존재 유무 확인
-//key를 반드시 문자열로 표기해야함.
-var ageFlag = 'age' in dog;
+// 프로퍼티 삭제
+delete cat.owner;
+console.log(cat);
+
+console.log('======================');
+
+// 프로퍼티 존재 유무 확인
+// key를 반드시 문자열로 표기해야함.
+
+var ageFlag = 'age123' in cat;
 console.log(ageFlag);
 
-//master 라는 프로퍼티가 없으면 추가해라
-if(!('master' in dog)){
-    dog.master = '김또수';
-}
-console.log(dog.master);
-
+// master라는 프로퍼티가 없으면 추가해라
 var m = 'master';
-if(!(m in dog)){
-    dog[m] = '김또또';
+
+if (!(m in cat)) {
+    cat[m] = '김또또';
 }
-console.log(dog[m]);
 
 
-//객체(key)를 순회하는 반복문
-for(var k in dog){
-    console.log(k);
+console.log('=========================');
+
+// 객체를 순회하는 반복문
+// key를 반복해서 추출해줌
+for (var k in dog) {
+    // console.log(typeof k);
     console.log(dog[k]);
 }
 
@@ -111,12 +128,20 @@ var articles = {
     ]
 };
 
+console.log('========================');
+console.log(articles.totalCount);
+console.log(articles.articleList[1].writer);
+console.log(articles.articleList[2].regDate);
+
 var bbs = articles.articleList;
+
 bbs[0].writer;
 
-for (var x in articles){
+var cnt = 0;
 
+for (var x in articles) {
+    console.log(`${++cnt}!!`);
 }
-for (var y of bbs){
-    console.log(y);
+for (var y of bbs) {
+
 }
