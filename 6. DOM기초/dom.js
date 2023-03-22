77d971296c09a93ec22086e3e79b3fe3712bd7d2
Youngsu Kim <126743125+Createcoding@users.noreply.github.com>
2023-03-22 15:04:10 +0900

    document

--- 6. DOM기초/dom.js
+++ 6. DOM기초/dom.js
@@ -1,2 +1,29 @@
 
 
+// <div id='box'>
+//   <input id='abc' class='zzz xxx vvv' type='text'>
+// </div
+
+//위를 풀어쓰면 아래처럼 되있다
+
+let box, input;
+box = {
+    tagName: 'div',
+    attributes: {
+        id: 'box'
+    },
+    children: [input],
+    // parentNode: document.body
+};
+
+input = {
+    tagName: 'input',
+    attributes: {
+        id: 'abc',
+        classList: ['zzz', 'xxx', 'vvv'],
+        type: 'text'
+    },
+    parentNode: box
+};
+
+console.log(input);
